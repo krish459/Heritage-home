74274cd4c4dff7e7fa3feb01d2e79099e3874f1b
Krish <krishshah235@gmail.com>
2023-03-23 15:39:47 +0530

    footer image

--- findhouse/src/components/common/footer/Footer.jsx
+++ findhouse/src/components/common/footer/Footer.jsx
@@ -8,8 +8,9 @@ const Footer = () => {
       <div className="col-sm-6 col-md-6 col-lg-3 col-xl-5 pr0 pl0">
         <div className="footer_about_widget">
           <h4>Heritage Homes</h4>
-          <p style={{color:"white"}}>
-          Heritage Homes blends modern technology with priceless heritage to preserve the past while moving towards a brighter future.
+          <p style={{ color: "white" }}>
+            Heritage Homes blends modern technology with priceless heritage to
+            preserve the past while moving towards a brighter future.
           </p>
         </div>
       </div>
@@ -51,20 +52,25 @@ const Footer = () => {
 
       <div className="col-sm-6 col-md-6 col-lg-3 col-xl-4">
         <div className="footer_contact_widget">
-          <h6>A PROJECT BY</h6>
-          <h4>AVD GROUP</h4>
-          <ul className="list-unstyled">
+          <h6 style={{color:"white"}}>A PROJECT BY</h6>
+          <div className="footerImage"
+          ></div>
+          <ul className="list-unstyled" >
             <li>
-              <span style={{color:"white"}}>111/B, Mandpeshwar Industrial Estate,</span>
+              <span style={{ color: "white" }}>
+                111/B, Mandpeshwar Industrial Estate,
+              </span>
             </li>
             <li>
-            <span style={{color:"white"}} >Opp. Prem Nagar,</span>
+              <span style={{ color: "white" }}>Opp. Prem Nagar,</span>
             </li>
             <li>
-            <span style={{color:"white"}} >Off. S. V. Road, Borivali (West)</span>
+              <span style={{ color: "white" }}>
+                Off. S. V. Road, Borivali (West)
+              </span>
             </li>
             <li>
-            <span style={{color:"white"}} >Mumbai, 400 092</span>
+              <span style={{ color: "white" }}>Mumbai, 400 092</span>
             </li>
           </ul>
         </div>
@@ -74,7 +80,12 @@ const Footer = () => {
           <h4>Contact Us</h4>
           <ul className="list-unstyled">
             <li>
-              <a href="mailto:heritagerealtors9@gmail.com"  style={{color:"white"}}>heritagerealtors9@gmail.com</a>
+              <a
+                href="mailto:heritagerealtors9@gmail.com"
+                style={{ color: "white" }}
+              >
+                heritagerealtors9@gmail.com
+              </a>
             </li>
             {/* <li>
               <a href="#">Collins Street West, Victoria</a>
@@ -83,10 +94,14 @@ const Footer = () => {
               <a href="#">8007, Australia.</a>
             </li> */}
             <li>
-              <a href="tel:+918169110810" style={{color:"white"}}>+91-8169110810</a>
+              <a href="tel:+918169110810" style={{ color: "white" }}>
+                +91-8169110810
+              </a>
             </li>
             <li>
-              <a href="tel:+918976708781" style={{color:"white"}}>+91-8976708781</a>
+              <a href="tel:+918976708781" style={{ color: "white" }}>
+                +91-8976708781
+              </a>
             </li>
           </ul>
         </div>
--- findhouse/src/components/home/Hero.jsx
+++ findhouse/src/components/home/Hero.jsx
@@ -2,7 +2,7 @@ import HeroFilter from "./HeroFilter";
 
 const Hero = () => {
   return (
-    <section className="home-one home1-overlay home1_bgi1" style={{backgroundImage: `url(${require('../../../public/assets/images/heri_Image/fullBuilding.jpg')})`}}>
+    <section className="home-one home1-overlay home1_bgi1">
       <div className="container">
         <div className="row posr">
           <div className="col-lg-12">
